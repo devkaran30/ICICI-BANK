7b091a123de9228cc1a40534387daf2ab223d081
raynav-3883 <raynav.kumar123@gmail.com>
2024-05-28 10:38:55 +0530

    final upload

--- script.js
+++ script.js
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     typeEffect();
   });
-  
+  //slide show pics
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+   
+   
